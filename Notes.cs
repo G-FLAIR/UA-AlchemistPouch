62ca50afd1df214e73e85553fe6c3ce4d5da9288
G-FLAIR <38269295+G-FLAIR@users.noreply.github.com>
2024-08-08 12:32:23 -0400

    Create Notes.cs
    Notes to work on GUMP

---
+++ Notes.cs
@@ -0,0 +1,11 @@
+Background reference : AddBackground( int x, int y, int width, int height, int gumpID )
+
+https://www.servuo.com/threads/gump-background-reference.4773/
+
+
+https://www.servuo.com/threads/change-the-text-of-a-gump.9098/
+
+HTML reference: 
+
+Add HTML : AddHtml(int x, int y, int width, int height, string text, bool background, bool scrollbar)
+
