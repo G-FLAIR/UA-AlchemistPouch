26745f41d4b69dde184666c015ec5450b676ea06
G-FLAIR <38269295+G-FLAIR@users.noreply.github.com>
2024-08-10 23:31:20 -0400

    Update Notes.cs

--- Notes.cs
+++ Notes.cs
@@ -1,14 +1,14 @@
-Background reference : AddBackground( int x, int y, int width, int height, int gumpID )
+// Background reference : AddBackground( int x, int y, int width, int height, int gumpID )
 
-https://www.servuo.com/threads/gump-background-reference.4773/
-
-
-https://www.servuo.com/threads/change-the-text-of-a-gump.9098/
-
-HTML reference: 
-
-Add HTML : AddHtml(int x, int y, int width, int height, string text, bool background, bool scrollbar)
+// https://www.servuo.com/threads/gump-background-reference.4773/
 
 
-There no Gump for potion and i believe that Potion color and name are fused during a certain script ... i'm missing something i guess
+// https://www.servuo.com/threads/change-the-text-of-a-gump.9098/
+
+// HTML reference: 
+
+// Add HTML : AddHtml(int x, int y, int width, int height, string text, bool background, bool scrollbar)
+
+
+// There no Gump for potion and i believe that Potion color and name are fabricated during a certain script. Using hex id didn't work ... i'm missing something i guess
 
