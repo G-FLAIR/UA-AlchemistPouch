2ee7d6db0a27b3af984ad3a2c0d3e1a8084bda6c
G-FLAIR <38269295+G-FLAIR@users.noreply.github.com>
2024-08-10 23:30:20 -0400

    Update Notes.cs

--- Notes.cs
+++ Notes.cs
@@ -9,3 +9,6 @@ HTML reference:
 
 Add HTML : AddHtml(int x, int y, int width, int height, string text, bool background, bool scrollbar)
 
+
+There no Gump for potion and i believe that Potion color and name are fused during a certain script ... i'm missing something i guess
+
